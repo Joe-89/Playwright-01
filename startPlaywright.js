8cae06a3973344e21b41a2868ead1f92f3df3a96
Josef Pokorný <josef.pokorny@unicodesys.cz>
2025-04-07 15:17:10 +0200

    Add initial Playwright setup and example test script

---
+++ startPlaywright.js
@@ -0,0 +1,34 @@
+/*
+1.-Instalace a vytvoření projektu
+npm init playwright@latest
+-Vyber TypeScript
+-Přidej GitHub Actions pokud chceš CI
+-Vytvoří ti strukturu projektu
+
+2.-Spuštění prvního testu
+npx playwright test
+
+Spuštění v UI režimu:
+npx playwright test --ui
+
+3.- Základy struktury testu
+// test.spec.ts
+
+import { test, expect } from '@playwright/test';
+
+test('zobrazí homepage', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
+
+*test(...)  -- definuje test
+*page.goto(...) -- otevře stránku
+*expect(...) -- porovnává výsledek s očekáváním
+
+4.-Lokátory
+ -jsou způsob, jak najít konkrétní prvky na stránce – text, atribut, třídu, CSS nebo XPath.
+
+await page.locator('text=Login').click();
+await page.locator('input[name="email"]').fill('pepa@test.cz');
+
+*/
